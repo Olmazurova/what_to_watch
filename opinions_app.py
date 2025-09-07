30e9eb8d36f678ef93a89d121ccd752c223c4588
Olga Mazurova <vsilena@mail.ru>
2025-09-07 16:58:55 +0300

    Доработаны шаблоны и вьюхи.

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from datetime import datetime
+from random import randrange
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    """Модель описывающая мнение о фильме."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    """Представление главной страницы, показывает случайное мнение о фильме."""
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных мнений о фильмах нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add')
+def add_opinion_view():
+    return render_template('add_opinion.html')
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get(id)
+    return render_template('opinion.html', opinion=opinion)
+
 
 if __name__ == '__main__':
     app.run()
