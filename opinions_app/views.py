75aa857936042e73304fa9a2d023ba6ddaae3815
Olga Mazurova <vsilena@mail.ru>
2025-09-10 13:07:24 +0300

    Написана вьюха и отдельная функция для получения случайного мнения.

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -6,14 +6,21 @@ from . import app, db
 from .models import Opinion
 from .forms import OpinionForm
 
+
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+
 @app.route('/')
 def index_view():
     """Представление главной страницы, показывает случайное мнение о фильме."""
-    quantity = Opinion.query.count()
-    if not quantity:
+    opinion = random_opinion()
+    if opinion is None:
         abort(500)
-    offset_value = randrange(quantity)
-    opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
 
 
